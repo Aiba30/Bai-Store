04d562839f63e46be26a30d0e5266688ff60058c
amina5789 <merzamedovaa@gmail.com>
2025-07-07 10:51:18 +0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { BrandSlider } from '@entities/brand/BrandSlider/BrandSlider'
-import { PopularSlide } from '@widgets/Category/PopularSlide/PopularSlide'
 import { About } from '@widgets/AboUt/AboUt'
+import { Footer } from '@shared/ui/Footer'
+import { PopularSlide } from '@widgets/Category'
+import { BrandSlider } from '@entities/brand'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <PopularSlide />
       <BrandSlider />
       <About />
+      <Footer />
     </>
   )
 }
